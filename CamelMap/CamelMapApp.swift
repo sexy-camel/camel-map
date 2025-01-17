858d833665a29b01d0120adc1364409d5b9ad37b
jhpark27 <jhpark27@nm-metalabs.com>
2025-01-17 16:01:41 +0900

    Initial Commit

---
+++ CamelMap/CamelMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamelMapApp.swift
+//  CamelMap
+//
+//  Created by 박종혁 on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CamelMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
